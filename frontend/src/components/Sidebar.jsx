5459597b6a46eecc58587f9fe82bde3658e1988c
Anuj Kumar <anujv8692@gmail.com>
2026-04-18 00:20:11 +0530

    changing to next level

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { NavLink, useNavigate } from "react-router-dom";
+import logonew from "../assets/logo-new.jpg";
 
 export default function Sidebar() {
 	const navigate = useNavigate();
@@ -10,7 +11,7 @@ export default function Sidebar() {
 	return (
 		<aside className="h-screen w-60 bg-gray-900 text-white flex flex-col shadow-lg">
 			<div className="p-6 text-2xl font-bold tracking-wide border-b border-gray-800">
-				<img src="" alt="" srcset="" />
+				<img src={logonew} alt="Logo" className="h-10 w-100 p-10"/>
 			</div>
 			<nav className="flex-1 p-4 space-y-2">
 				<NavLink to="/" className={({isActive}) => isActive ? "block p-3 rounded bg-blue-700" : "block p-3 rounded hover:bg-gray-800"} end>
